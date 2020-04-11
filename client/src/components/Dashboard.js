33388c1c6a5a272caf8f11f2fcb53ba025260c18
Michel Bronco Kekerovic <michelpunchcode@gmail.com>
2020-04-11 14:26:45 -0700

    subject choices on dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,10 +6,74 @@ export default (props) => {
     <div className="dashboard">
       <aside className="sidebar">
         <nav>
-          <a href="#">curriculum</a>
+          <div></div>
+          <a href="#">CURRICULUM</a>
+          <div>
+            <ul>
+              <li>
+                a href="/">Art
+                <a />
+              </li>
+              <li>
+                a href="/">Biology
+                <a />
+              </li>
+              <li>
+                a href="/">Business Studies
+                <a />
+              </li>
+              <li>
+                a href="/">Chemistry
+                <a />
+              </li>
+              <li>
+                a href="/">Computer Science
+                <a />
+              </li>
+              <li>
+                a href="/">Design and Technology
+                <a />
+              </li>
+              <li>
+                a href="/">Economics
+                <a />
+              </li>
+              <li>
+                a href="/">English
+                <a />
+              </li>
+              <li>
+                a href="/">Foreign Languages
+                <a />
+              </li>
+              <li>
+                a href="/">Information Technology
+                <a />
+              </li>
+              <li>
+                a href="/">Mathematics
+                <a />
+              </li>
+              <li>
+                a href="/">Music
+                <a />
+              </li>
+              <li>
+                a href="/">Physics
+                <a />
+              </li>
+              <li>
+                a href="/">Psychology
+                <a />
+              </li>
+              <li>
+                a href="/">Sociology
+                <a />
+              </li>
+            </ul>
+          </div>
         </nav>
       </aside>
-      <div>SectionOne</div>
       <main className="main">
         <nav className="topnav"></nav>
         <div className="pdf">
